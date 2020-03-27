3c65702e23de6a592f20ded55ca35987b99a980e
pittachalk <>
2020-03-27 15:44:12 +0000

    test svd algorithms

--- sc3s-testings.py
+++ sc3s-testings.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import math
 
 sc.settings.verbosity = 3
 sc.logging.print_versions()
@@ -76,7 +77,8 @@ sc.pl.umap(adata, color='leiden')
 
 adata.X.shape
 
-import math
+def decompose_matrix_svd(X, no_components)
+
 
 def streamSC(data, k = 3, batchmode = False, initial = 0.2,
              stream = 0.02, lowrankdim = 0.05, iterations = 5,
@@ -91,12 +93,16 @@ def streamSC(data, k = 3, batchmode = False, initial = 0.2,
     assert streammin <= streammax
     
     # randomise ordering of cells
-    
-    
+    # expr_matrix = np.transpose(data.X) 
     
     # obtain and calculate useful values
     # there's quite some edge cases that I need to think about for this part
     n_cells, n_genes = data.X.shape
+
+    # calculate low rank representation size
+    # we want the representation of cells in a lower dimensionality space, not of genes
+    # I actually think rounding up is a mistake, should round down and throw an error if needed
+    lowrankdim = math.ceil(lowrankdim * n_cells)
 
     # calculate size of initial stream
     if n_cells < initialmin + 1:
@@ -117,6 +123,17 @@ def streamSC(data, k = 3, batchmode = False, initial = 0.2,
     # choose the SVD solver
     # need to factor in random seed
     
+    # initialise empty array for "previous batch"
+    # row: genes, cell: low rank dim
+    B = np.empty((n_genes, lowrankdim))
+    #U = [] # rotation matrix (don't think this is needed, this is not Julia)
+    
+    # for consensus results
+    # these is for nParallele executions of B    
+    #compM1 = zeros(nParallel, lowRankCells+1, maxK);
+    
+    # centroids
+    # to come
     
     # do the streaming
     i = 0
@@ -131,126 +148,171 @@ def streamSC(data, k = 3, batchmode = False, initial = 0.2,
         else:
             j = stream
         
-        # obtain current batch and increment counter
-        print(i, i + j)
+        # obtain current batch, concatenate to previous batch and increment counter
+        current_cells = adata.X[i:(i+j), ]
+        print(i, i + j, current_cells.shape)
+        
+        # should consider putting the first batch outside, because the size is so different?
+        # would also make it easier to benchmark later on
+        if i == 0:
+            
+        # need deepcopy?
+        
         i += j
+        
+        # get U and B from previous time step (need to deepcopy?)
+        if i
+        
+        """
+        # get U and B from previous step
+        Uold = deepcopy(U); Bold = deepcopy(B);
+
+        # append the new stream, otherwise use the first batch 
+        # compM1 is denoted as C_t in the paper
+        if ~isempty(B)
+            compM1 = convert(Array{Float64, 2}, hcat(B, current_cells)); #step5 of alg5
+        else
+            compM1 = current_cells;
+        end
+        
+        # perform the SVD
+        tmp = svd(compM1);
+        U = tmp.U[:,1:lowRankCells];
+        S = tmp.S[1:lowRankCells];
+        V = tmp.V[:,1:lowRankCells];
+
+        # normalise the sigma singular values (step 7 of strmEMB)
+        delta = S[lowRankCells]^2;
+        Sn = diagm(0 => max.((S.^2 .- delta), 0).^(1/2));
+        Bfull = real(U * Sn);
+        """
         
     # unrandomise the ordering
     print("stream finished...")
 
 
-adata.X.shape
-from scipy import linalg
-U, s, Vh = linalg.svd(adata.X, full_matrices=False)
-U.shape,  s.shape, Vh.shape
-reconstructed = np.dot(U, np.dot(np.diag(s), Vh))
+#######################################################
+# testing svd methods
 
-l = round(0.5 * adata.X.shape[1])# low rank
+adata.X.shape
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from scipy import linalg
+
+# low rank dimension
+l = round(0.5 * adata.X.shape[1])
+
+U, s, Vh = linalg.svd(adata.X, full_matrices=False)
+U.shape,  s.shape, Vh.shape # (n_cells, n_genes), (n_genes,), (n_genes, n_genes)
+
+reconstructed_full = np.dot(U, np.dot(np.diag(s), Vh))
 reconstructed_less = np.dot(U[:,:l], np.dot(np.diag(s[:l]), Vh[:l,]))
 
 # check similarity
-np.allclose(adata.X, reconstructed, atol = 1e-3)
-np.sum((reconstructed - adata.X) ** 2) # rmse
+np.allclose(adata.X, reconstructed_full, atol = 1e-3)
+error_full = reconstructed_full - adata.X
+np.sum(error_full ** 2) # rmse
+sns.distplot(error_full)
 
 np.allclose(adata.X, reconstructed_less, atol = 1)
+error_less = reconstructed_less - adata.X
 np.sum((reconstructed_less - adata.X) ** 2) # rmse
+sns.distplot(error_less)
 
-import plotly.io as pio
-pio.renderers.default = "browser"
-import plotly.express as px
-from plotly.subplots import make_subplots
+# i can't get this subplot to work for some reason
+f, axes = plt.subplots(2, 1, figsize=(7, 7), sharex=True)
+sns.despine(left=True)
+sns.distplot(error_full, color="b", ax=axes[0, 0])
+sns.distplot(reconstructed_less - adata.X, hist=False, rug=True, color="r", ax=axes[1, 0])
+plt.setp(axes, yticks=[])
+plt.tight_layout()
 
-px.imshow(adata.X)
-px.imshow(reconstructed)
-px.imshow(reconstructed_less)
-
-#fig = make_subplots(rows = 1, cols = 3)
-#fig.append_trace(px.imshow(reconstructed_less), row=1, col=1) #doesn't work
-#fig
-
-# NOW to test the truncated SVD
-
-
-
-
-################################################################################
-################################################################################
-
-
-adata.obs['consensus'] = adata.obs['leiden']
-test_palette = ['black'] + adata.uns['leiden_colors'][1:]
-
-# nice! very easy to customise
-sc.pl.umap(adata, color='consensus', palette=test_palette)
 
 
 ##########
 
-# FINDING MARKER GENES
-
-# compute a ranking for the highly DE genes in each cluster
-# done with the t-test
-sc.tl.rank_genes_groups(adata, 'leiden', method='t-test')
-sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
-
-# alternative is the Wilcoxon rank-sum test (recommended)
-sc.tl.rank_genes_groups(adata, 'leiden', method='wilcoxon')
-sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
-adata.write(results_file)
-
-# we can also use more unconventional multivariate tests
-# like a logistic regression
-sc.tl.rank_genes_groups(adata, 'leiden', method='logreg')
-sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
-
-# thus we decide that:
-marker_genes = ['IL7R', 'CD79A', 'MS4A1', 'CD8A', 'CD8B', 'LYZ', 'CD14',
-                'LGALS3', 'S100A8', 'GNLY', 'NKG7', 'KLRB1',
-                'FCGR3A', 'MS4A7', 'FCER1A', 'CST3', 'PPBP']
+# NOW to test the truncated SVD
+# http://blog.explainmydata.com/2016/01/how-much-faster-is-truncated-svd.html
 
 
-# reload the object saved with the Wilcoxon rank-sum test
-adata = sc.read(results_file)
+# Truncated SVD with scikit-learn
+from sklearn.decomposition import TruncatedSVD
 
-# show the top 10 ranked genes per cluster 0,1,...,7
-pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(5)
+# randomised version
+l = round(0.2 * adata.X.shape[1])
+svd_random = TruncatedSVD(l, algorithm="randomized", random_state=None)
+U2  = svd_random.fit_transform(adata.X) # this has already been scaled by the singular vectors
+Vh2 = svd_random.components_ 
+s2  = svd_random.singular_values_ 
+U2_uns = U2 / np.tile(s2, (U2.shape[0], 1))
+reconstructed2 = svd_random.inverse_transform(U2) # you need to keep the svd_random object
+reconstructed2_manual = np.dot(U2, Vh2)
 
-result = adata.uns['rank_genes_groups']
-groups = result['names'].dtype.names
-pd.DataFrame(
-    {group + '_' + key[:1]: result[key][group]
-     for group in groups for key in ['names', 'pvals']}).head(5)
+np.round(U2/U[:,0:l])[:,0:5]
+np.round(Vh2/Vh[0:l],1)[0:5,]
+s2[0:5]
 
-# compare to a single cluster (here we're comparing clusters 2 and 3 with 1)
-sc.tl.rank_genes_groups(adata, 'leiden', groups=['2','3'], reference='1', method='wilcoxon')
-sc.pl.rank_genes_groups(adata, groups=['2', '3'], n_genes=20)
+np.round(U2 / U[:,0:l])[:,-4:]
+np.round(Vh2/Vh[0:l],1)[-4:,]
+s2[-4:]
 
-# violin plots
-sc.pl.rank_genes_groups_violin(adata, groups=['2','3'], n_genes=8)
+# middle bit
+m = l//4 *3
+np.round(U2 / U[:,0:l])[:,m:(m+5)]
+np.round(Vh2/Vh[0:l],1)[m:(m+5),]
+s2[m:(m+5)]
 
-# compare a gene across groups
-sc.pl.violin(adata, ['CST3', 'NKG7', 'PPBP'], groupby='leiden')
 
-# actually mark the cell types
-new_cluster_names = [
-    'CD4 T', 'CD14 Monocytes',
-    'B', 'CD8 T',
-    'NK', 'FCGR3A Monocytes',
-    'Dendritic', 'Megakaryocytes', 'why is there an extra cluster']
-adata.rename_categories('leiden', new_cluster_names)
+error2 = reconstructed2 - adata.X
+np.sum(error2 ** 2) # rmse
+sns.distplot(error2)
 
-sc.pl.umap(adata, color='leiden', legend_loc='on_data', title='', frameon=False)
 
-# visualise the marker genes
-ax = sc.pl.dotplot(adata, marker_genes, groupby='leiden')
-ax = sc.pl.stacked_violin(adata, marker_genes, groupby='leiden', rotation=90)
+# arpack version
+svd_arpack = TruncatedSVD(l, algorithm="arpack", random_state=None, tol=0.0)
+U3  = svd_arpack.fit_transform(adata.X)
+Vh3 = svd_arpack.components_
+s3  = svd_arpack.singular_values_ 
+reconstructed3 = svd_random.inverse_transform(U3) # you need to keep the svd_random object
 
-# save the annotations
-adata
-adata.write(results_file, compression="gzip")
 
-# you can also save the file without the dense and scaled data matrix
-# the sparse matrix is saved as .raw
-adata.X = None
-adata.write('./write/pbmc3k_withoutX.h5ad', compression='gzip')
+error3 = reconstructed3 - adata.X
+np.sum(error3 ** 2) # rmse
+sns.distplot(error3)
 
+
+# compare images
+import plotly.io as pio
+pio.renderers.default = "browser"
+import plotly.express as px
+px.imshow(adata.X)
+px.imshow(reconstructed_less)
+px.imshow(reconstructed2)
+px.imshow(reconstructed2_manual)
+px.imshow(reconstructed3)
+
+
+def np_svd(X):
+    return np.linalg.svd(X, full_matrices=False, compute_uv=True)
+
+def np_inv_svd(svd_outputs):
+    U, s, V = svd_outputs
+    return np.dot(U, np.dot(np.diag(s), V))
+
+def sklearn_randomized_svd(X, rank=RANK):
+    tsvd = sklearn.decomposition.TruncatedSVD(rank, algorithm="randomized", n_iter=1)
+    X_reduced = tsvd.fit_transform(X)
+    return (tsvd, X_reduced)
+
+def sklearn_arpack_svd(X, rank=RANK):
+    tsvd = sklearn.decomposition.TruncatedSVD(rank, algorithm="arpack")
+    X_reduced = tsvd.fit_transform(X)
+    return (tsvd, X_reduced)
+
+def sklearn_inv_svd(svd_outputs):
+    tsvd, X_reduced = svd_outputs
+    return tsvd.inverse_transform(X_reduced)
+
+def decompose_matrix()
