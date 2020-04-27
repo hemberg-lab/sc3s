2c809d883fdf718bc7126ee612fe9bec1812d903
pittachalk <>
2020-04-27 17:25:44 +0100

    organise into an sc3s module

---
+++ sc3s/_utils.py
@@ -0,0 +1,46 @@
+import numpy as numpy
+import pandas as pd
+import math
+from scipy import linalg
+from sklearn.decomposition import TruncatedSVD
+from sklearn.cluster import KMeans as _skkmeams
+from scipy.cluster.vq import kmeans2 as kmeans
+
+def calculate_rmse(A, B):
+    """
+    Calculate root mean squared error between two matrices.
+    """
+    error = A - B
+    return np.sum(error ** 2)
+
+
+def svd_scipy(X, n_components):
+    """
+    Singular value decomposition using `scipy.linalg.svd`.
+    Returned matrices are truncated to the value of `n_components`.
+    """
+    U, s, Vh = linalg.svd(X, full_matrices=False)
+    U  = U[:, :n_components]
+    s  = s[:n_components]
+    Vh = Vh[:n_components, ]
+    return U, s, Vh
+
+
+def inv_svd(U, s, Vh):
+    """
+    Inverse of the singular value decomposition.
+    """
+    return np.dot(U, np.dot(np.diag(s), Vh))
+
+
+def svd_sklearn(X, n_components, n_iter=5, random_state=None):
+    """
+    Truncated singular value decomposition using `scikitlearn`.
+    """
+    svd = TruncatedSVD(n_components, algorithm="randomized", n_iter=n_iter,
+                       random_state=random_state)
+    U = svd.fit_transform(X)
+    s = svd.singular_values_
+    Vh = svd.components_
+    U = U / np.tile(s, (U.shape[0],1)) # by default, U is scaled by s
+    return U, s, Vh
---
+++ sc3s/tools.py
@@ -0,0 +1,5 @@
+from ._cluster import strm_spectral, create_embedding, update_embedding, strm_kmeans
+from ._utils import calculate_rmse
+
+def consensus_clustering(x):
+    pass
