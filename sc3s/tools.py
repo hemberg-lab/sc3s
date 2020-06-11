bf1d676e2034e883c67638961dafc811aecd4db6
pittachalk <>
2020-06-11 12:01:30 +0100

    add contingency and hclust

--- sc3s/tools.py
+++ sc3s/tools.py
@@ -43,10 +43,10 @@ def consensus_clustering_legacy(
     assert _check_iterable(num_clust), "pls ensure num_clust is a list"
 
     for K in num_clust:
-        for i in range(0, len(lowrankrange)):
-            # empty dictionary to hold the clustering results
-            clusterings = {}
+        # empty dictionary to hold the clustering results
+        clusterings = {}
 
+        for i in range(0, len(lowrankrange)):
             # run spectral clustering, parameters modified to use the whole batch
             runs = strm_spectral(adata.X, k=K, n_parallel=n_parallel,
                 streammode=False, svd_algorithm=svd_algorithm, initial=adata.X.shape[0],
@@ -54,10 +54,12 @@ def consensus_clustering_legacy(
                 lowrankdim = lowrankrange[i])
             clusterings.update(runs)
 
-            # perform the consensus clusterings
-            clusterings = {k: v['asgn'] for k,v in clusterings.items()}
-            result = _combine_clustering_runs(clusterings, K)
-            _write_results_to_anndata(result, adata, num_clust=K, prefix='sc3ori_')
+        # perform the consensus clusterings
+        clusterings = {k: v['asgn'] for k, v in clusterings.items()}
+        result = _combine_clustering_runs_legacy(clusterings, K)
+        _write_results_to_anndata(result, adata, num_clust=K, prefix='sc3ori_')
+    
+    return clusterings
 
 #from sklearn.cluster import KMeans
 #import pandas as pd ???
@@ -77,3 +79,18 @@ def _combine_clustering_runs(clusterings, num_clust):
 def _consolidate_microclusters(clusterings, num_clust):
     # take dict of clustering runs and consolidate with weighted k-means
     return {k: weighted_kmeans(run['cent'], run['asgn'], num_clust) for k, run in clusterings.items()}
+
+from sklearn.cluster import AgglomerativeClustering
+def convert_clusterings_to_contigency(clusterings):
+    X = np.array([x for x in clusterings.values()]).T   # row: cell, col: clustering run
+    C = np.zeros((X.shape[0], X.shape[0]))
+    for i in np.arange(0, X.shape[0]):
+        C[i,] = np.sum(X[i,:] == X, axis=1) / X.shape[1]
+    assert np.allclose(C, C.T), "contingency matrix not symmetrical"
+    return C
+
+def _combine_clustering_runs_legacy(clusterings, num_clust):
+    # convert to contingency-based consensus, then does hierarchical clustering
+    consensus_matrix = convert_clusterings_to_contigency(clusterings)
+    hclust = AgglomerativeClustering(n_clusters=num_clust, linkage='complete').fit(consensus_matrix)
+    return pd.Categorical(hclust.labels_)
