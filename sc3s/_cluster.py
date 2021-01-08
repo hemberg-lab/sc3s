806a6bb434d9dac421371849a6a70c9b7e6aaf19
pittachalk <>
2021-01-08 17:03:11 +0000

    Revert "fix n_runs error message in runminibatchKmeans"
    This reverts commit 1298be9b4b94b02501e2f1568f7e7ae7b9c4f1a6.

--- sc3s/_cluster.py
+++ sc3s/_cluster.py
@@ -29,7 +29,7 @@ def run_trials_miniBatchKMeans(data, n_clusters, d_range, n_runs, batch_size, ra
     )
 
     # check n_runs
-    n_runs = _check_integer_single(n_runs, min_val=1, var_name="n_runs")
+    assert isinstance(n_runs, int) and n_runs > 1, "n_runs must be positive integer value."
 
     # check batch_size
     batch_size = _check_integer_single(
