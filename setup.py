500afadb519d5fceedc585e662d26222f83ec9cd
Simon Murray <simondonaldmurray@gmail.com>
2021-02-04 16:16:15 +0000

    added author_email, url to setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ setup(
     name="sc3s", 
     version="0.3.0",
     description="Python Single Cell Consensus Clustering",
+    url="https://github.com/pittachalk/sc3s",
     author="Fu Xiang Quah",
-    packages=find_packages(),
-    install_requires=[
-       "numpy>=1.19.1", 
-       "pandas>=1.1.3", 
-       "scikit-learn>=0.23.2"
-    ],
+    author_email="fxq20@cam.ac.uk",
     classifiers=[
        "Programming Language :: Python :: 3",
        "License :: OSI Approved :: BSD License",
        "Operating System :: OS Independent"
     ],
-    python_requires=">=3.6"    
+    packages=find_packages(),
+    python_requires=">=3.6",    
+    install_requires=[
+       "numpy>=1.19.1", 
+       "pandas>=1.1.3", 
+       "scikit-learn>=0.23.2"
+    ]
 )
