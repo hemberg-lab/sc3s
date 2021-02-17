aa21b37c54b91214c5be4584f918a3205dba78dc
Simon Murray <simondonaldmurray@gmail.com>
2021-02-17 09:46:04 +0000

    reverted setup.py to match requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sc3s", 
-    version="0.3.0",
+    version="0.0.1-dev",
     description="Python Single Cell Consensus Clustering",
     url="https://github.com/pittachalk/sc3s",
     author="Fu Xiang Quah",
@@ -17,6 +17,7 @@ setup(
     install_requires=[
        "numpy>=1.19.1", 
        "pandas>=1.1.3", 
-       "scikit-learn>=0.23.2"
-    ]
+       "scikit-learn>=0.23.2",
+       "scanpy>=1.6.0" 
+  ]
 )
