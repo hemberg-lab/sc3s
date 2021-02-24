cd129aa9de8af9e76e3ec726b9ae66719f06062b
Simon Murray <simondonaldmurray@gmail.com>
2021-02-24 13:39:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,18 +16,16 @@ setup(
     python_requires=">=3.6",    
     install_requires=[
        "numpy>=1.19.2,<2",
-       "pandas>=1.2.2",
+       "pandas>=1.1.5",
        "python>=3.6",
        "scikit-learn>=0.23.2",
        "scanpy>=1.7.0",
        "six>=1.15.0",
        "setuptools>=52.0.0",
        "joblib>=1.0.0",
-       "mkl-service>=2.3.0",
        "h5py >=3.1.0",
        "seaborn",
        "scipy",
        "python-dateutil >=2.8.1",
-       "openssl >=1.0.1",
   ]
 )
