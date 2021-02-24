699b6ea6b26359b2a2ab651bef1621435c5dfec8
Simon Murray <simondonaldmurray@gmail.com>
2021-02-24 11:26:30 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sc3s", 
-    version="0.0.1-dev",
+    version="0.0.1-dev1",
     description="Python Single Cell Consensus Clustering",
     url="https://github.com/pittachalk/sc3s",
     author="Fu Xiang Quah",
@@ -15,9 +15,19 @@ setup(
     packages=find_packages(),
     python_requires=">=3.6",    
     install_requires=[
-       "numpy>=1.19.1", 
-       "pandas>=1.1.3", 
+       "numpy>=1.19.2,<2",
+       "pandas>=1.2.2",
+       "python>=3.6",
        "scikit-learn>=0.23.2",
-       "scanpy>=1.6.0" 
+       "scanpy>=1.7.0",
+       "six>=1.15.0",
+       "setuptools>=52.0.0",
+       "joblib>=1.0.0",
+       "mkl-service>=2.3.0",
+       "h5py >=3.1.0",
+       "seaborn",
+       "scipy",
+       "python-dateutil >=2.8.1",
+       "openssl >=1.0.1",
   ]
 )
